4cc9f361ce88d7d67d1a5d03f458cf5fa5ad80de
Nicholas Todoroff <todorof3@msu.edu>
2019-02-04 16:44:05 -0500

    Make getGiabData.sh show timings

--- homework/HW3/getGiabData.sh
+++ homework/HW3/getGiabData.sh
@@ -1,2 +1,16 @@
-wget -P sequences ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz
-wget -P sequences ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
+#!/bin/bash
+
+url_base='ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome'
+urls[0]="$url_base/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz"
+urls[1]="$url_base/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz"
+
+getdata() {
+  wget -P sequences
+  wget -P sequences Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
+}
+
+tot_time=$(TIME='%E' time for url in "${urls[@]}"; do
+  wget -nv --show-progress -P "$url"
+done)
+
+echo "Total time: $tot_time"
