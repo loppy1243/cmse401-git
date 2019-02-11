eb0cd1e2c74938e2ef489c18ec0101d1239bd7b7
Nicholas Todoroff <todorof3@msu.edu>
2019-02-11 18:22:09 -0500

    HW3: Make `getGiabData.sh` and `getRefGnm.sh` better

--- homework/HW3/getGiabData.sh
+++ homework/HW3/getGiabData.sh
@@ -4,13 +4,7 @@ url_base='ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG00
 urls[0]="$url_base/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz"
 urls[1]="$url_base/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz"
 
-getdata() {
-  wget -P sequences
-  wget -P sequences Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
-}
-
-tot_time=$(TIME='%E' time for url in "${urls[@]}"; do
-  wget -nv --show-progress -P "$url"
-done)
-
-echo "Total time: $tot_time"
+local TIMEFORMAT=$'\nTotal Time: %R'
+time for url in "${urls[@]}"; do
+    wget -nv --show-progress -P sequences "$url"
+done
--- homework/HW3/getRefGnm.sh
+++ homework/HW3/getRefGnm.sh
@@ -3,5 +3,4 @@
 url='ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/dna'
 url+='/Homo_sapiens.GRCh38.dna.toplevel.fa.gz'
 
-tot_time=$(TIME='%E' time wget -nv --show-progress -P sequences "$url")
-echo "Total time: $tot_time"
+TIMEFORMAT=$'\nTotal Time: %R' time wget -nv --show-progress -P sequences "$url"
