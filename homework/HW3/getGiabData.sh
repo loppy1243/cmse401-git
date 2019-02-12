bd303064171168f52bdfe4613f594a49a2e94f9e
Nicholas Todoroff <todorof3@msu.edu>
2019-02-12 16:51:00 -0500

    Final versions of getGiabData.sh and getRefGnm.sh

--- homework/HW3/getGiabData.sh
+++ homework/HW3/getGiabData.sh
@@ -4,7 +4,10 @@ url_base='ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG00
 urls[0]="$url_base/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz"
 urls[1]="$url_base/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz"
 
-local TIMEFORMAT=$'\nTotal Time: %R'
-time for url in "${urls[@]}"; do
-    wget -nv --show-progress -P sequences "$url"
-done
+TIMEFORMAT=$'Time: %Rs'
+time {
+  for i in ${!urls[@]}; do
+    time wget -nv -P sequences "${urls[$i]}"
+  done
+TIMEFORMAT=$'\nTotal Time: %Rs'
+}
--- homework/HW3/getRefGnm.sh
+++ homework/HW3/getRefGnm.sh
@@ -3,4 +3,5 @@
 url='ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/dna'
 url+='/Homo_sapiens.GRCh38.dna.toplevel.fa.gz'
 
-TIMEFORMAT=$'\nTotal Time: %R' time wget -nv --show-progress -P sequences "$url"
+TIMEFORMAT=$'\nTotal Time: %R'
+time wget -nv -P sequences "$url";
