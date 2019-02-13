2dfc7413866f57fe600fecab5fd311747d47a06f
Nicholas Todoroff <todorof3@msu.edu>
2019-02-13 12:50:13 -0500

    HW3: Change getRefGnm.sh to get worm geonome

--- homework/HW3/getRefGnm.sh
+++ homework/HW3/getRefGnm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-url='ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/dna'
-url+='/Homo_sapiens.GRCh38.dna.toplevel.fa.gz'
+url='ftp://ftp.ensembl.org/pub/release-95/fasta/caenorhabditis_elegans/dna'
+url+='/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz'
 
 TIMEFORMAT=$'\nTotal Time: %R'
 time wget -nv -P sequences "$url";
