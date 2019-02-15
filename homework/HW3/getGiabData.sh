d6a02e8c4819b66fb32e4427d0407fe29aae0b42
Nicholas Todoroff <todorof3@msu.edu>
2019-02-14 20:57:36 -0500

    HW3: Change getGiabData.sh to get correct examples, remove timings from download scripts

--- homework/HW3/getGiabData.sh
+++ homework/HW3/getGiabData.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-url_base='ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome'
-urls[0]="$url_base/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz"
-urls[1]="$url_base/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz"
-
-TIMEFORMAT=$'Time: %Rs'
-time {
-  for i in ${!urls[@]}; do
-    time wget -nv -P sequences "${urls[$i]}"
-  done
-TIMEFORMAT=$'\nTotal Time: %Rs'
-}
+cp /mnt/scratch/colbrydi/BWA_EXAMPLE_DATA/* sequences/
--- homework/HW3/getRefGnm.sh
+++ homework/HW3/getRefGnm.sh
@@ -3,5 +3,4 @@
 url='ftp://ftp.ensembl.org/pub/release-95/fasta/caenorhabditis_elegans/dna'
 url+='/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz'
 
-TIMEFORMAT=$'\nTotal Time: %R'
-time wget -nv -P sequences "$url";
+wget -P sequences "$url";
