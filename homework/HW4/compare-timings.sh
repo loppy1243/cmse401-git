0c4a9dec13f5d0e6a164c51d02bd0ba5e79c6e2a
Nicholas Todoroff <loppy1243@gmail.com>
2019-02-24 14:54:14 -0500

    HW4: Make compare-timings.sh useful

--- homework/HW4/compare-timings.sh
+++ homework/HW4/compare-timings.sh
@@ -1,16 +1,75 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 <image> <timing_set1> <timing_set2> [timing_set3 ...]"
+
+if (( $# != 3 )); then
+  echo "Usage: $0 <image> <timing_set1> <timing_set2>"
   exit 1
 fi
 
 img="$1"
 shift
 
-files=""
+files=()
 for s in "$@"; do
-  files+=" timings/compiled/$s/$img.dat"
+  files+=("timings/compiled/$s/$img.dat")
 done
 
-paste -d '|' $files | column -ts '|' -o ' | '
+tmpdir=$(mktemp -d)
+cleanup() {
+  rm -rf "$tmpdir"
+  (( $# != 0 )) && kill "-SIG$1" $$
+}
+trap cleanup EXIT
+for sig in HUP TERM INT; do trap "cleanup $sig" $sig; done
+
+script1=$(cat <<EOF
+FNR == 1 {
+  printf "%s", "$1"
+  for (i=0; i < NF; ++i) printf " ."
+  printf " |\n"
+  print ".", \$0, "|"
+}
+FNR != 1 { print ".", \$0, "|" | "sort -bk 1,1" }
+EOF
+)
+
+script2=$(cat <<EOF
+FNR == 1 {
+  printf "%s", "$2"
+  for (i=0; i < NF; ++i) printf " ."
+  printf "\n"
+  print ".", \$0
+}
+FNR != 1 { print ".", \$0 | "sort -bk 1,1" }
+EOF
+)
+
+awk -e "$script1" "${files[0]}" >"$tmpdir/table1"
+awk -e "$script2" "${files[1]}" >"$tmpdir/table2"
+join --header -j2 -a1 -a2 "$tmpdir/table1" "$tmpdir/table2" >"$tmpdir/joined"
+
+cat "$tmpdir/joined" | column -tR "7,14"
+echo
+
+tot_times=($(awk -e '/^CUMULATIVE/ { print $4 }' "${files[@]}"))
+script3=$(cat <<EOF
+BEGIN {
+  tot_left = ${tot_times[0]}
+  tot_right = ${tot_times[1]}
+
+  print "Name", "%$1", "%$2", "diff", "tval"
+}
+
+FNR >= 3 {
+  no_l = \$5 == ""
+  no_r = \$12 == ""
+
+  print \$1,
+        no_l ? "N/A" : sprintf("%.2f", \$5/tot_left*100),
+        no_r ? "N/A" : sprintf("%.2f", \$12/tot_right*100),
+        no_l || no_r ? "N/A" : sprintf("%.3e", \$5-\$12),
+        no_l || no_r ? "N/A" : sprintf("%.2f", (\$5-\$12)/sqrt(\$6^2/\$5+\$13^2/\$11))
+}
+EOF
+)
+awk -e "$script3" "$tmpdir/joined" | column -tR "$(seq -s, 2 4)"
