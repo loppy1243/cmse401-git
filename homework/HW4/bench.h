2dd461ca1d2ffb65169f8f16956369cf83760c7f
Nicholas Todoroff <loppy1243@gmail.com>
2019-03-03 14:23:27 -0500

    Change bench.h to use wall time instead of CPU time

--- homework/HW4/bench.h
+++ homework/HW4/bench.h
@@ -3,31 +3,31 @@
 
 #ifdef BENCH
     #include <stdio.h>
-    #include <time.h>
+    #include <omp.h>
 
     #define IFDEF_BENCH(x) x
-    clock_t bench_total_clock = 0;
+    double bench_total_clock = 0.0;
 #else
     #define IFDEF_BENCH(x)
 #endif
 
 #define _START_CLOCK(name) \
-    clock_t name = clock();
+    double name = omp_get_wtime();
 #define START_CLOCK(name) IFDEF_BENCH(_START_CLOCK(name))
 
 #define _STOP_CLOCK(name) \
-    name = clock() - name; \
+    name = omp_get_wtime() - name; \
     bench_total_clock += name;
 #define STOP_CLOCK(name) IFDEF_BENCH(_STOP_CLOCK(name))
 
 #define _PRINT_CLOCK(name) \
-    printf("CLOCK %s: %gs\n", #name, (name)/(double) CLOCKS_PER_SEC);
+    printf("CLOCK %s: %gs\n", #name, (name));
 #define PRINT_CLOCK(name) IFDEF_BENCH(_PRINT_CLOCK(name))
 
 #define STOP_PRINT_CLOCK(name) STOP_CLOCK(name); PRINT_CLOCK(name);
 
 #define _PRINT_TOTAL_CLOCK \
-    printf("CLOCK CUMULATIVE: %gs\n", bench_total_clock/(double) CLOCKS_PER_SEC);
+    printf("CLOCK CUMULATIVE: %gs\n", bench_total_clock);
 #define PRINT_TOTAL_CLOCK IFDEF_BENCH(_PRINT_TOTAL_CLOCK)
 
 #endif // _BENCH_H defined
