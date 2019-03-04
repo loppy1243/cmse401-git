8a1eb7aa486fc2920a0f4546f5024d34c7cf5f6b
Nicholas Todoroff <loppy1243@gmail.com>
2019-03-04 13:32:50 -0500

    HW4: Fix typo in run-thread-group.sh running guided timings too many times

--- homework/HW4/run-thread-group.sh
+++ homework/HW4/run-thread-group.sh
@@ -2,5 +2,5 @@ OMP_SCHEDULE=static ./gen-timings.sh omp-$1-static 40
 for i in {0..7}; do
   k=$(( 2**$i ))
   OMP_SCHEDULE=dynamic,$k ./gen-timings.sh omp-$1-dynamic-$k 40
-  OMP_SCHEDULE=guided ./gen-timings.sh omp-$1-guided 40
 done
+OMP_SCHEDULE=guided ./gen-timings.sh omp-$1-guided 40
