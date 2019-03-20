69b2fc7cd398954cb3cc1b1c01893cd21baf2288
Nicholas Todoroff <loppy1243@gmail.com>
2019-03-19 20:39:39 -0400

    Generalize hist.py

--- homework/HW5/hist.py
+++ homework/HW5/hist.py
@@ -1,34 +1,67 @@
-import sys
+import argparse
+import os
 import csv
 import numpy as np
 from matplotlib import pyplot as plt
 
-prefix = 'timings/compiled/'+sys.argv[1]
-nodes = ['dev-intel14-k20', 'dev-intel16-k80']
+parser = argparse.ArgumentParser()
+parser.add_argument('-x', dest='excluded_fields', type=str, action='append',
+                    help='exclude the specified field from the plot', default=[])
+parser.add_argument('-o', dest='outfile', type=str, help='output file')
+parser.add_argument('timing_sets', type=str, nargs='+',
+                    help='timings sets to include in the plot')
+args = parser.parse_args()
 
-walltimes = {}
-cputimes = {}
-for node in nodes:
-    walltimes[node] = []
-    cputimes[node] = []
-    with open(prefix+'/'+node+'.dat', newline='') as strm:
-        reader = csv.reader(strm, delimiter=' ', skipinitialspace=True)
-        next(reader)
-        for row in reader:
-            walltimes[node].append(float(row[1]))
-            cputimes[node].append(float(row[2]))
+prefix = 'timings/compiled/'
+nodes = os.listdir(prefix+'/'+args.timing_sets[0])
+for i in range(len(nodes)):
+    nodes[i] = '.'.join(nodes[i].split('.')[:-1])
 
-ax = plt.subplot(2, 1, 1)
+times = {}
+fields = []
+all_fields = None
 for node in nodes:
-    plt.hist(walltimes[node], label=node)
-    plt.title('Serial Wall Times')
-    plt.legend()
+    times[node] = {}
+    for tset in args.timing_sets:
+        times[node][tset] = {}
+        with open(prefix+'/'+tset+'/'+node+'.dat', newline='') as strm:
+            reader = csv.reader(strm, delimiter=' ', skipinitialspace=True)
+            if fields == []:
+                all_fields = next(reader)
+                for field in all_fields:
+                    if field not in args.excluded_fields:
+                        fields.append(field)
+                        times[node][tset][field] = []
+            else:
+                next(reader)
+                for field in fields:
+                    times[node][tset][field] = []
+            for row in reader:
+                for field, val in zip(all_fields, row):
+                    if field not in args.excluded_fields:
+                        times[node][tset][field].append(float(val))
 
-plt.subplot(2, 1, 2, sharex=ax)
-for node in nodes:
-    plt.hist(cputimes[node], label=node)
-    plt.title('Serial CPU Times')
-    plt.legend()
+plot_dims = (len(fields), len(args.timing_sets))
+splot_x, splot_y = 1, 1
+def splot(x, y):
+    return x + (y-1)*plot_dims[1]
+
+for tset in args.timing_sets:
+    first = True
+    ax = None
+    for field in fields:
+        if first:
+            first = False
+            ax = plt.subplot(*plot_dims, splot(splot_x, splot_y))
+        else:
+            plt.subplot(*plot_dims, splot(splot_x, splot_y), sharex=ax)
+
+        for node in nodes:
+            plt.hist(times[node][tset][field], label=node)
+        plt.title(tset+' '+field)
+        plt.legend()
+        splot_y = splot_y % (plot_dims[0]) + 1
+    splot_x = splot_x % (plot_dims[1]) + 1
 
 plt.tight_layout()
-plt.savefig(sys.argv[2])
+plt.savefig(args.outfile)
