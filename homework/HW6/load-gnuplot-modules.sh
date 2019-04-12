3aa50cc627180dde308eec0e96a37dd9d2734a22
Nicholas Todoroff <loppy1243@gmail.com>
2019-04-12 02:18:49 -0400

    hw6-dev: Change load-gnuplot.modules.sh -> load-plot-modules.sh, add Python module

--- homework/HW6/load-gnuplot-modules.sh
+++
@@ -1,3 +0,0 @@
-# This file must be sourced; do NOT run as a script!
-
-module load GNU/6.4.0-2.28 OpenMPI/2.1.2 gnuplot/5.2.2
---
+++ homework/HW6/load-plot-modules.sh
@@ -0,0 +1,3 @@
+# This file must be sourced; do NOT run as a script!
+
+module load GNU/6.4.0-2.28 OpenMPI/2.1.2 gnuplot/5.2.2 Python/3.6.4
